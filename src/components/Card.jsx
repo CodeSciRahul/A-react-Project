2869bc479f5d49be19841740e719a4d1c86e75f2
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-02-28 18:33:59 +0530

    al most completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { FaWheelchairMove } from "react-icons/fa6";
-const Card = () => {
+import { MdOutlineFileDownload } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import { motion } from "framer-motion"
+
+const Card = ({data,refernce}) => {
   return (
     <>
-     <div className=' relative m-4 w-64 bg-zinc-700/80 h-72 rounded-[30px] text-white px-8 py-10 overflow-hidden'>
+     <motion.div  drag dragConstraints= {refernce} whileDrag={{scale:1.1}} className=' relative m-4 w-64 bg-zinc-700/80 h-72 rounded-[30px] text-white px-8 py-10 overflow-hidden'>
         <FaWheelchairMove />
-        <p className='py-5 leading-tight text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam incidunt fuga quas.</p>
-        <div className='absolute bottom-0 left-0  w-[100%] bg-sky-400 h-14'></div>
+        <p className='py-5 leading-tight text-sm'>{data.desc}</p>
+        <div className='absolute bottom-5 left-0  w-[100%] px-8 flex justify-between mb-6'>
+          <span>{data.filesize}</span>
+          {data.isDownload ?<MdOutlineFileDownload/>: <RxCross2/> } 
         </div>
+        {data.tag.isOpen && <div className={`absolute bottom-0 left-0 w-[100%] py-2 text-center ${data.tag.bg.blue ? "bg-sky-700":"bg-green-700"}`}>Download</div>}
+        
+        </motion.div>
     </>
   )
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card';
 
 const Foreground = () => {
+  const data = [
+    {
+      desc: "hello i love you himanshi. i miss you a lot every single day i miss you.",
+      filesize: "0.8mb",
+      isDownload: true,
+      tag: {
+        isOpen: true,
+        bg: {
+          blue: false,
+        }
+      }
+    },
+    {
+      desc: "hello i love you himanshi. i miss you a lot every single day i miss you.",
+      filesize: "0.55mb",
+      isDownload: false,
+      tag: {
+        isOpen: false,
+        bg: {
+          blue: false,
+        }
+      }
+    },
+    {
+      desc: "hello i love you himanshi. i miss you a lot every single day i miss you.",
+      filesize: "0.75mb",
+      isDownload: true,
+      tag: {
+        isOpen: true,
+        bg: {
+          blue: true,
+        }
+      }
+    }
+  ];
+  const ref =useRef(null)
+
+
   return (
     <>
-    <div className='fixed z-[3] bg-transparent w-full h-screen'>
-      <Card/>
-    </div>
+      <div ref = {ref} className='fixed z-[3] bg-transparent w-full h-screen flex'>
+        {data.map((item, index) => (
+          <Card key={index} data={item} refernce = {ref}/>
+        ))}
+      </div>
     </>
   )
 }
